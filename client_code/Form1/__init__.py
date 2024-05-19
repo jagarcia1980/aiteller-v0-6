27396a2e0ff9d6b88eb6f1bf491bba896218c45a
jagarcia1980@gmail.com <jagarcia1980@gmail.com>
2024-05-19 17:06:02 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -9,3 +9,9 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.rich_text_1.content="<b>negrita</b><h3>encabezado</h3>"
+    pass
+    
