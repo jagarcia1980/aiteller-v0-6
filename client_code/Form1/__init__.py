2e2df07db0db398fca3438030d1d02ad25aff96c
jagarcia1980@gmail.com <jagarcia1980@gmail.com>
2024-05-24 22:30:53 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -12,6 +12,15 @@ class Form1(Form1Template):
 
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
+    
     self.rich_text_1.content="<b>negrita</b><h3>encabezado</h3>"
+    pass
+
+  def button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    
+    self.rich_text_1.content=anvil.server.call('callable_paso1_2', 
+      self.txtcodigo.text,self.txtlugar.text,self.txttematica.text)
+
     pass
     
